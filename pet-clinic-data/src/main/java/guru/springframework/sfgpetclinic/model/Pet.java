0194afa92a630f8a431c9c74ffaf1f165187fe11
François VONGVILAY <f.vongvilay@gmailc.om>
2019-03-28 01:54:54 +0100

    Added Pets and Contact info to owners on startup. closes #29

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 
 public class Pet extends BaseEntity{
 
+    private String name;
+
     private PetType petType;
 
     private Owner owner;
@@ -16,6 +18,14 @@ public class Pet extends BaseEntity{
         this.petType = petType;
         this.owner = owner;
         this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public PetType getPetType() {
